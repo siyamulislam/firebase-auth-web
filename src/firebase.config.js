5cdeb75492eb6f54cc7548e6a52c483b566c409d
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-01-16 21:11:28 +0600

    initial added

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB5925_wiTKrn9-7UM2QFX1Gvdg9bQYDQ0",
+    authDomain: "ema-jhon-2022.firebaseapp.com",
+    projectId: "ema-jhon-2022",
+    storageBucket: "ema-jhon-2022.appspot.com",
+    messagingSenderId: "1070815504848",
+    appId: "1:1070815504848:web:6658bdcf39c2fdba03b1d5"
+  };
+  
+  export default firebaseConfig;
